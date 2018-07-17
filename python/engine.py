9e6006b652050dd8ce7ec3f81a6d69bcc3a96f97
Paulo Menezes <paulo.hgmenezes@gmail.com>
2018-07-16 23:12:21 -0300

    Roguelike tutorial part 9

--- python/engine.py
+++ python/engine.py
@@ -7,7 +7,7 @@ from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_messages import Message, MessageLog
 from game_states import GameStates
-from input_handlers import handle_keys
+from input_handlers import handle_keys, handle_mouse
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all, RenderOrder
 
@@ -70,6 +70,8 @@ def main():
 	game_state = GameStates.PLAYERS_TURN
 	previous_game_state = game_state
 
+	targeting_item = None
+
 	while not libtcod.console_is_window_closed():
 		libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
@@ -83,6 +85,7 @@ def main():
 		clear_all(con, entities)
 
 		action = handle_keys(key, game_state)
+		mouse_action = handle_mouse(mouse)
 
 		move = action.get('move')
 		pickup = action.get('pickup')
@@ -91,6 +94,9 @@ def main():
 		inventory_index = action.get('inventory_index')
 		exit = action.get('exit')
 		fullscreen = action.get('fullscreen')
+
+		left_click = mouse_action.get('left_click')
+		right_click = mouse_action.get('right_click')
 
 		player_turn_results = []
 
@@ -133,13 +139,33 @@ def main():
 			item = player.inventory.items[inventory_index]
 
 			if game_state == GameStates.SHOW_INVENTORY:
-				player_turn_results.extend(player.inventory.use(item))
+				print('use 1')
+				abc = player.inventory.use(item, entities=entities, fov_map=fov_map)
+				print(abc)
+				player_turn_results.extend(abc)
 			elif game_state == GameStates.DROP_INVENTORY:
 				player_turn_results.extend(player.inventory.drop_item(item))
+
+		if game_state == GameStates.TARGETING:
+			if left_click:
+				target_x, target_y = left_click
+
+				print('use 2')
+				item_use_results = player.inventory.use(targeting_item, entities=entities, fov_map=fov_map, target_x=target_x, target_y=target_y)
+
+				player_turn_results.extend(item_use_results)
+			elif right_click:
+				player_turn_results.append({
+					'targeting_cancelled': True
+				})
 
 		if exit:
 			if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
 				game_state = previous_game_state
+			elif game_state == GameStates.TARGETING:
+				player_turn_results.append({
+					'targeting_cancelled': True
+				})
 			else:
 				return True
 
@@ -152,6 +178,10 @@ def main():
 			item_added = player_turn_result.get('item_added')
 			item_consumed = player_turn_result.get('consumed')
 			item_dropped = player_turn_result.get('item_dropped')
+			targeting = player_turn_result.get('targeting')
+			targeting_cancelled = player_turn_result.get('targeting_cancelled')
+
+			print(targeting)
 
 			if message:
 				message_log.add_messages(message)
@@ -176,6 +206,18 @@ def main():
 				entities.append(item_dropped)
 
 				game_state = GameStates.ENEMY_TURN
+
+			if targeting:
+				previous_game_state = GameStates.PLAYERS_TURN
+				game_state = GameStates.TARGETING
+
+				targeting_item = targeting
+
+				message_log.add_messages(targeting_item.item.targeting_message)
+
+			if targeting_cancelled:
+				game_state = previous_game_state
+				message_log.add_messages(Message('Targeting cancelled'))
 
 		if game_state == GameStates.ENEMY_TURN:
 			for entity in entities:
